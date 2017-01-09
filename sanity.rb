3c3ebf20eb1e7dca81276b95025b5b6fbad73e6f
Jason Yeo <jasonyeo88@gmail.com>
2017-01-09 15:55:29 +0800

    Use tarball

--- sanity.rb
+++ sanity.rb
@@ -3,10 +3,11 @@ require "formula"
 class Sanity < Formula
   desc "A simple framework that helps you maintain sanity before releasing your code"
   homepage "https://github.com/jsyeo/sanity"
-  url "https://github.com/jsyeo/sanity/releases/download/v0.0.1/sanity-mac-osx-0.0.1"
-  sha256 "c824c5da14ac81a7da8265c42d6d9f3a8586b1670fa094585d48e2f6bc226a8b<Paste>"
+  url "https://github.com/jsyeo/sanity/releases/download/v0.0.1/sanity-mac-osx-0.0.1.tgz"
+  sha256 "2b2a746affd09cb0dabb5f43580c7c266a0c8c52ff5ee185d2dfc9b35542332a"
 
   def install
+    libexec.install Dir["*"]
     bin.install_symlink libexec/"bin/sanity"
   end
 end
