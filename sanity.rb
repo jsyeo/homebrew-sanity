f105e27da47c12f5f336cf365e0d642ff8b9e4ee
Jason Yeo <jasonyeo88@gmail.com>
2017-01-11 14:25:52 +0800

    Update formula do fetch v0.0.3

--- sanity.rb
+++ sanity.rb
@@ -3,8 +3,8 @@ require "formula"
 class Sanity < Formula
   desc "A simple framework that helps you maintain sanity before releasing your code"
   homepage "https://github.com/jsyeo/sanity"
-  url "https://github.com/jsyeo/sanity/releases/download/v0.0.1/sanity-mac-osx-0.0.1.tgz"
-  sha256 "2b2a746affd09cb0dabb5f43580c7c266a0c8c52ff5ee185d2dfc9b35542332a"
+  url "https://github.com/jsyeo/sanity/releases/download/v0.0.3/sanity-mac-osx-0.0.3.tgz"
+  sha256 "e4fcbd4bc8363e93413bfb2956887616f9a15746cf91ebf44a360a0ed9f54990"
 
   def install
     bin.install Dir["sanity"]
